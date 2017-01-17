12ea4a615fd308ee86a5c31e5a95b4adfdfb86ff
RshihkaiD-SourceTree <kevin.chang@tgc-taiwan.com.tw>
2017-01-17 10:29:35 +0800

    add receiver manager include config status

--- js/receiver.js
+++ js/receiver.js
@@ -1,6 +1,20 @@
-(function(){
-	window.mediaElement = document.getElementById('setMedia');
-	window.mediaManager = new cast.receiver.MediaManager(window.mediaElement);
-	window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
-	window.castReceiverManager.start();
+(function() {
+    window.mediaElement = document.getElementById('setMedia');
+    window.mediaManager = new cast.receiver.MediaManager(window.mediaElement);
+    window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
+    window.castReceiverManager.start();
+
+    window.castReceiverManager.onSenderDisconnected = function(event) {
+        if (window.castReceiverManager.getSenders().length == 0 &&
+            event.reason == cast.receiver.system.DisconnectReason.REQUESTED_BY_SENDER) {
+            window.close();
+        }
+    }
+
+    /* -- API Config -- */
+    var apiConfig = new cast.receiver.CastReceiverManager.Config();
+    apiConfig.statusText = '準備播放';
+    apiConfig.maxInactivity = 6000;
+    window.castReceiverManager.start(apiConfig);
+
 })();
