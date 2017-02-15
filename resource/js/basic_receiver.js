6756532e53ca0158da61df0ade39fc326b3fcf7d
RshihkaiD-SourceTree <kevin.chang@tgc-taiwan.com.tw>
2017-02-15 16:54:43 +0800

    no message

--- resource/js/basic_receiver.js
+++
@@ -1,9 +0,0 @@
-window.mediaElement = document.getElementById('receiverMedia');
-window.mediaManager = new cast.receiver.MediaManager(window.mediaElement);
-window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
-window.castReceiverManager.start();
-window.castReceiverManager.onSenderDisconnected = function(event){
-    if(window.castReceiverManager.getSenders().length == 0 && event.reason == cast.receiver.system.DisconnectReason.REQUESTED_BY_SENDER){
-        window.close();
-    }
-}
