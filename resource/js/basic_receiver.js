a45a55c9cd0edc300ba04f624f8c5d0cba69bd60
RshihkaiD-SourceTree <kevin.chang@tgc-taiwan.com.tw>
2017-02-15 14:39:42 +0800

    test codes

---
+++ resource/js/basic_receiver.js
@@ -0,0 +1,4 @@
+window.mediaElement = document.getElementById('receiverMedia');
+window.mediaManager = new cast.receiver.MediaManager(window.mediaElement);
+window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
+window.castReceiverManager.start();
